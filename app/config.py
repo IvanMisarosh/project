558d41e49dc08314d88af095cfbe1d50a98b0c00
Ivan Misarosh <ivan.misarosh05@gmail.com>
2025-03-27 19:45:58 +0200

    added necessary configurations for deployment

--- app/config.py
+++ app/config.py
@@ -2,10 +2,16 @@ import os
 from dotenv import load_dotenv
 
 # Load environment variables from .env file
-load_dotenv()
+# load_dotenv()
 
 class Settings:
-    DATABASE_URL: str = os.getenv("DATABASE_URL")
-    DEBUG: bool = os.getenv("DEBUG", "False").lower() in ["true", "1"]
+    if os.getenv("WEBSITE_HOSTNAME"):
+        DATABASE_URL: str = os.getenv("AZURE_POSTGRESQL_CONNECTIONSTRING")
+        if not DATABASE_URL:
+            print("Missing environment variable: AZURE_POSTGRESQL_CONNECTIONSTRING")
+    else:
+        load_dotenv()
+        DATABASE_URL: str = os.getenv("DATABASE_URL")
+        DEBUG: bool = os.getenv("DEBUG", "False").lower() in ["true", "1"]
 
 settings = Settings()
