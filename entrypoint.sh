4df04408d590ea97afae69ca8e3055d9663e0010
Ivan Misarosh <ivan.misarosh05@gmail.com>
2025-03-27 20:10:08 +0200

    updated entrypoint script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 python3 -m pip install --upgrade pip
-python3 -m pip install -e app
+python3 -m pip install -r requirements.txt  
 alembic upgrade head
 python3 -m gunicorn app.main:app -c gunicorn.conf.py
